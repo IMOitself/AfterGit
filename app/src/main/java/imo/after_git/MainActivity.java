d8db622d9facc046c1a02cd380bdeb591f37e79c
IMOitself <russjonn@gmail.com>
2025-06-21 00:14:46 +0800

    feat: detect and take action if git is not installed

--- app/src/main/java/imo/after_git/MainActivity.java
+++ app/src/main/java/imo/after_git/MainActivity.java
@@ -1,20 +1,21 @@
 package imo.after_git;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.Settings;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import imo.after_run.CommandTermux;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity 
 {
@@ -44,7 +45,6 @@ public class MainActivity extends Activity
 				if (! CommandTermux.backgroundMode) instruction.setVisibility(View.VISIBLE);
 				String repoPath = repoPathEdit.getText().toString().trim();
 				String command = "cd " + repoPath;
-				command += "\ngit config --global --add safe.directory " + repoPath;
 				command += "\ngit status";
 				
 				outputTxt.setText("wait..");
@@ -52,7 +52,52 @@ public class MainActivity extends Activity
                     .setOnDetect(new Runnable(){
                         @Override
                         public void run(){
-                            outputTxt.setText(CommandTermux.OutputDetector.output);
+                            String output = CommandTermux.OutputDetector.output;
+                            outputTxt.setText(output);
+                            
+                            boolean isWorking = output.contains("On branch");
+                            
+                            if(isWorking) return;
+                            
+                            if(output.contains("git: not found")) {
+                                LinearLayout messageLayout = new LinearLayout(MainActivity.this);
+                                TextView messageText = new TextView(MainActivity.this);
+                                Button copyBtn = new Button(MainActivity.this);
+                                
+                                messageText.setText("type this on Termux to install git:\n\npkg install git -y");
+                                copyBtn.setOnClickListener(new View.OnClickListener(){
+                                    @Override
+                                    public void onClick(View v){
+                                        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                                        if (clipboard == null) return;
+                                        ClipData clip = ClipData.newPlainText("Copied Text", "pkg install git -y");
+                                        clipboard.setPrimaryClip(clip);
+
+                                        Toast.makeText(MainActivity.this, "Text copied to clipboard", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                
+                                messageLayout.setOrientation(LinearLayout.VERTICAL);
+                                messageLayout.addView(messageText);
+                                messageLayout.addView(copyBtn);
+                                
+                                new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("Git not installed")
+                                    .setView(messageLayout)
+                                    .setNegativeButton("Open Termux", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dia, int which) {
+                                            try {
+                                                CommandTermux.openTermux(MainActivity.this);
+                                            } catch(Exception e) {}
+                                        }
+                                    })
+                                    .create().show();
+                                return;
+                            }
+
+                            //TODO: run command "\ngit config --global --add safe.directory "
+                            //      if current repoPath is not set yet
                         }
                     })
                     .run();
