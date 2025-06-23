e1a84220ae9d1c68ae858b055ece803b5c7c61b4
IMOitself <russjonn@gmail.com>
2025-06-23 13:48:51 +0800

    fix: output text still use 'load button' that is already been renamed
    to 'status'

--- app/src/main/java/imo/after_git/MainActivity.java
+++ app/src/main/java/imo/after_git/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends Activity
     @Override
     protected void onResume() {
         super.onResume();
-        outputTxt.setText("click load button");
+        outputTxt.setText("click status button to load repository folder");
     }
     
     void runGitStatus(final String repoPath, final TextView outputTxt){
